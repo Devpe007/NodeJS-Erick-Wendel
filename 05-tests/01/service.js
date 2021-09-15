61d64021646a38a4357debcb8f6e20be6e298160
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-09-15 11:10:22 -0300

    created service star wars API with axios

---
+++ 05-tests/01/service.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+const URL = `https://swapi.dev/api/people`;
+
+async function getPeople(name) {
+    const url = `${URL}/?search=$${name}$format=json`;
+    const result = await axios.default.get(url);
+    return result.data;
+};
+
+module.exports = { getPeople };
